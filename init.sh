a91d32ca2f1feb3e7c6262ea5451fce474814bce
Jacob Schaupp <jacob.schaupp@gmail.com>
2026-01-10 09:35:31 -0500

    More build install fixes

--- init.sh
+++ init.sh
@@ -1,13 +1,13 @@
-git submodule init
-git submodule update
-
-mkdir -p extern/BasisUniversal/build
-
-cd extern/BasisUniversal/build
+#!/bin/bash
+mkdir -p extern/BasisUniversal
+cd extern/BasisUniversal
+rm -rf build
+mkdir -p build
+cd build
 
 cmake -DCMAKE_CXX_STANDARD=17 -DCMAKE_BUILD_TYPE=Release ..
-
 cmake --build . --config Release
+
 cd ../../../
 mkdir -p deps
 mkdir -p deps/BasisUniversal
